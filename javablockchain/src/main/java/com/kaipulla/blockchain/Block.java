c12bac43658bc7074d321da7a3e5f1396d576422
Rajendran, Tamil <trajendran@stubhub.com>
2017-12-26 12:10:20 -0500

    adding to chain

--- javablockchain/src/main/java/com/kaipulla/blockchain/Block.java
+++ javablockchain/src/main/java/com/kaipulla/blockchain/Block.java
@@ -1,23 +1,44 @@
 package com.kaipulla.blockchain;
 
+import java.util.Date;
 import java.util.List;
 
 public class Block {
 
-    private int hash;
+    private Integer index;//where is this Block in the chain
+    private Date timestamp;//what time was this block created?
+    private int previousHash;
     private List<Transaction> transactions;
 
-    public Block(int hash, List<Transaction> transactions) {
-        this.hash = hash;
+    public Block(Integer index, Date timestamp, int previousHash, List<Transaction> transactions) {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.previousHash = previousHash;
         this.transactions = transactions;
     }
 
-    public int getHash() {
-        return hash;
+    public Integer getIndex() {
+        return index;
     }
 
-    public void setHash(int hash) {
-        this.hash = hash;
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getPreviousHash() {
+        return previousHash;
+    }
+
+    public void setPreviousHash(int previousHash) {
+        this.previousHash = previousHash;
     }
 
     public List<Transaction> getTransactions() {
@@ -30,7 +51,9 @@ public class Block {
 
     @Override
     public int hashCode() {
-        int result = hash;
+        int result = previousHash;
+        result = 31 * result + (index != null ? index.hashCode() : 0);
+        result = 31 * result + (timestamp != null ? timestamp.hashCode() : 0);
         result = 31 * result + (transactions != null ? transactions.hashCode() : 0);
         return result;
     }
--- javablockchain/src/main/java/com/kaipulla/blockchain/BlockChain.java
+++ javablockchain/src/main/java/com/kaipulla/blockchain/BlockChain.java
@@ -1,17 +1,30 @@
 package com.kaipulla.blockchain;
 
 import java.util.Arrays;
+import java.util.Date;
 
 public class BlockChain {
+
+    public static Integer counter = 0;
 
     public static void main(String ... args) {
         Transaction t1 = new Transaction("tamil", "baskar", 20.0);
         Transaction t2 = new Transaction("baskar", "vijay", 50.0);
         Transaction t3 = new Transaction("baskar", "vijay", 50.0);
         System.out.println(t1.hashCode()+" "+t2.hashCode()+" "+t3.hashCode());
-        Block firstBlock = new Block(0, Arrays.asList(t1, t2));
-        Block next = new Block(firstBlock.hashCode(), Arrays.asList(t3));
+        Date timestamp = new Date();
+        Block firstBlock = new Block(0, timestamp, 0, Arrays.asList(t1, t2));
+        BlockChain chain = new BlockChain();
+        chain.addToChain(firstBlock);
+        timestamp = new Date();
+        Block next = new Block(counter, timestamp, firstBlock.hashCode(), Arrays.asList(t3));
+        chain.addToChain(next);
         System.out.println(firstBlock.hashCode()+" "+next.hashCode());
+        System.out.println("Length of this chain is: "+counter);
+    }
+
+    public synchronized void addToChain(Block block) {
+        counter = block.getIndex() + 1;
     }
 
 }
