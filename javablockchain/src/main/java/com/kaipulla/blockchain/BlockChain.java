03cee8c0eaa7ebca367ebc23f7cba47cb051e9af
Rajendran, Tamil <trajendran@stubhub.com>
2017-12-26 13:07:38 -0500

    added validity check

--- javablockchain/src/main/java/com/kaipulla/blockchain/BlockChain.java
+++ javablockchain/src/main/java/com/kaipulla/blockchain/BlockChain.java
@@ -14,4 +14,18 @@ public class BlockChain {
     public synchronized void addToChain(Block block) {
         blocks.add(block);
     }
+
+    public boolean isValid() {
+        if(blocks != null && blocks.size() > 0) {
+            for (int i = 1; i < blocks.size(); i++) {
+                Block first = blocks.get(i);
+                Block prev = blocks.get(i-1);
+
+                if(first.getPreviousHash() != prev.hashCode()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
